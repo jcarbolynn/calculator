fcb942918d47aa82097ca3ec19876e298833fb45
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-03 13:11:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,50 @@
-def addition(n1, n2):
-  """add 2 numbers"""
+#Calculator
+def add(n1, n2):
   return n1 + n2
 
 def subtract(n1, n2):
-  """subtract n2 from n1"""
   return n1 - n2
 
 def multiply(n1, n2):
-  """multiply two numbers"""
   return n1 * n2
 
 def divide(n1, n2):
-  """divide n1 by n2"""
   return n1 / n2
 
 operations = {
-  "+": "addition",
-  "-": "subtract",
-  "*": "multiply",
-  "/": "divide",
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
 
-num1 = int(input("What's the first number: "))
-for op in operations:
-  print(op)
-operation_symbol = input("Pick an operation from above: ")
-num2 = int(input("What's the second number: "))
-answer = locals()[operations[operation_symbol]](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-#do not understand locals() at all but it worked? lets you use a string to call a function
-#but also OMG i got it in my operations dictionary I do NOT have to make the definitions strings...
+num1 = int(input("What's the first number?: "))
+for symbol in operations:
+  print(symbol)
 
-'''
+#Here we select "+"
+operation_symbol = input("Pick an operation: ") 
+num2 = int(input("What's the next number?: "))
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-'''
+first_answer = calculation_function(num1, num2)
 
-#str object not callable, but I checked and operations[operation_symbol] makes a string? can't use a string to call a function?
-#print(locals(operations[operation_symbol](num1, num2)))
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
 
-'''
-if operation_symbol == "+":
-  index = 0
-elif operation_symbol == "-":
-  index = 1
-elif operation_symbol == "*":
-  index = 2
-elif operation_symbol == "/":
-  index = 3
+#Here we select "*" which overides the "+" we selected on line 26.
+operation_symbol = input("Pick an operation: ") 
+num3 = int(input("What's the next number?: "))
 
-listofoperations = ["addition", "subtract", "multiply", "divide"]
-print(listofoperations[index](num1, num2))
-'''
+#Here the calculation_function selected will be the multiply() function
+calculation_function = operations[operation_symbol] 
+
+#Here the code will be:
+#second_answer = multiply(multiply(num1, num2), num3)
+second_answer = calculation_function(calculation_function(num1, num2), num3)
+#second_answer = 2 * 3 * 3 = 18
+#To fix this bug we need to change the code on line 42 to:
+second_answer = calculation_function(first_answer, num3)
+#In the next lesson, we will delete all the code from line 34-48 so don't worry
+#It won't affect your final project.
+#But it's a good oportunity to practice debugging. 🐞
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
